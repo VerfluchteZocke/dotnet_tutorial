c29e43909c919c7222a85310e743bb8db8b3d55c
Sandra Borst <108298114+VerfluchteZocke@users.noreply.github.com>
2024-05-16 12:17:59 +0200

    wwwroot folder added

--- Program.cs
+++ Program.cs
@@ -7,22 +7,10 @@ namespace WebApplication2
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.Use(async (context, next) =>
-            {
-                await context.Response.WriteAsync("Middleware1: Incoming Request\n");
-                await next();
-                await context.Response.WriteAsync("Middleware1: Outgoing Response\n");
-
-
-            });
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Middleware2: Incoming Request handled and response generated\n");
-                //Terminal Middleware Component i.e. cannot call the Next Component
-            });
-
-            //app.MapGet("/", () => $"Worker Process Name : {System.Diagnostics.Process.GetCurrentProcess().ProcessName}");
+            app.MapGet("/", () => $"EnvironmentName: {app.Environment.EnvironmentName} \n" +
+            $"ApplicationName: {app.Environment.ApplicationName} \n" +
+            $"WebRootPath: {app.Environment.WebRootPath} \n" +
+            $"ContentRootPath: {app.Environment.ContentRootPath}");
 
             app.Run();
         }
