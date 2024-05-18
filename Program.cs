0dd39e9331ae36ea79cbd39328f7844b09048552
Sandra Borst <108298114+VerfluchteZocke@users.noreply.github.com>
2024-05-18 12:02:17 +0200

    developer exception page

--- Program.cs
+++ Program.cs
@@ -7,11 +7,19 @@ namespace WebApplication2
             var builder = WebApplication.CreateBuilder(args);
             var app = builder.Build();
 
-            app.UseDirectoryBrowser();
-            app.UseStaticFiles();
+            if (app.Environment.IsDevelopment())
+            {
+                DeveloperExceptionPageOptions developerExceptionPageOptions = new DeveloperExceptionPageOptions
+                {
+                    SourceCodeLineCount = 5
+                };
+                app.UseDeveloperExceptionPage(developerExceptionPageOptions);
+            }
 
             app.Run(async (context) =>
             {
+                int Number1 = 10, Number2 = 0;
+                int Result = Number1 / Number2;
                 await context.Response.WriteAsync("Request Handled and Response Generated");
             });
             //This will Run the Application
